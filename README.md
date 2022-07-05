ede4f18534d3889820852e050fd33d04577d5392
Suryashashank G <gsuryashashank@gmail.com>
2022-07-05 17:12:23 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Making a Simple React App
+
+Making a simple expense tracker.
---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h2>Hallo wassup</h2>
+      <p1>This is a paragraph</p1>
+    </div>
+  );
+}
+
+export default App;
