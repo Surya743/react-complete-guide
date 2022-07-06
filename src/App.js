991594adfead82e165288b2cbe32bc317cfa3553
Suryashashank G <gsuryashashank@gmail.com>
2022-07-06 10:01:01 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
   return (
-    <div>
-      <h2>Lets get Started!</h2>
-      <ExpenseItem></ExpenseItem>
-      
-    </div>
+    <ExpenseItem></ExpenseItem>
   );
 }
 
